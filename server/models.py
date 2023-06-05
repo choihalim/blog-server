a24fe8a76f255feea031dccf760ae34f4cc7bf4f
Halim Choi <hchoicareer@gmail.com>
2023-06-05 14:35:34 -0400

    created db

--- server/models.py
+++ server/models.py
@@ -2,4 +2,54 @@ from sqlalchemy_serializer import SerializerMixin
 
 from config import db
 
-# Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    avatar = db.Column(db.String)
+    password = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+    
+class Post(db.Model, SerializerMixin):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    body = db.Column(db.String)
+    type = db.Column(db.String)
+    tags = db.Column(db.String)
+    likes = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f'<Post {self.title}>'
+
+class Comment(db.Model, SerializerMixin):
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    likes = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+    def __repr__(self):
+        return f'<Comment {self.id}>'
+
+class Follower(db.Model, SerializerMixin):
+    __tablename__ = "followers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    following_user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    followed_by_user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
